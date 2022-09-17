358dde7cefbfaf5f95cb9aef85eb6f0f44e79327
plitche <kysu728@naver.com>
2022-09-17 21:47:12 +0900

    $emit('update-name', e.target.value)_03

--- test1/src/components/InputField.vue
+++ test1/src/components/InputField.vue
@@ -3,16 +3,16 @@
         <label for="">Name</label>
         <input 
             type="text" 
-            :value="name"
+            :value="value"
             style="padding: 30px; border: 2px solid green;"
-            @input="$emit('update-name', $event)"/>
+            @input="$emit('input', $event.target.value)"/>
     </div>
 </template>
 
 <script>
 export default {
     props: {
-        name: {
+        value: {
             type: String,
             required: true
         }
