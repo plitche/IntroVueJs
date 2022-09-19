bf7fbd0243596c598137c2a2d2f73cc702bb12a6
plitche <kysu728@naver.com>
2022-09-19 23:29:55 +0900

    20. Todo App 1_01

---
+++ todo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input type="text" class="w-100 p-2" placeholder="Type todo"/>
+    <hr/>
+    <div>
+      <input type="checkbox" />
+      <span class="m-3">Buy a car</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
